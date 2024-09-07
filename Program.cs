5de4d64135066dd3a8a883a7626cd668f2fa243b
Marcos A. Nascimento <marcos.nascimento@gmail.com>
2024-09-06 22:36:58 -0300

    commit inicial

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
+Console.Clear();
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero:"11 987654321", modelo:"Modelo 1", imei:"11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero:"12 987651234", modelo:"Modelo 2", imei:"2222222", memoria:128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
